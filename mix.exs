5174ea3a8d74d9f382a27f0d60e9c22623ad6a4a
Derek Kraan <derek.kraan@gmail.com>
2017-06-23 15:50:03 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,11 +3,11 @@ defmodule Bunck.Mixfile do
 
   def project do
     [app: :bunck,
-     version: "0.1.0",
-     build_path: "../../_build",
-     config_path: "../../config/config.exs",
-     deps_path: "../../deps",
-     lockfile: "../../mix.lock",
+     version: "0.1.1",
+     build_path: "_build",
+     config_path: "config/config.exs",
+     deps_path: "deps",
+     lockfile: "mix.lock",
      elixir: "~> 1.4",
      package: package(),
      description: "A productive Elixir client for the Bunq API",
