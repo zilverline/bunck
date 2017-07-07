d219d276cf012b7a3f7e36d3a649d1bf33cf923b
Derek Kraan <derek.kraan@gmail.com>
2017-07-07 10:43:04 +0200

    Add documentation for Enumerable

--- lib/bunck.ex
+++ lib/bunck.ex
@@ -1,27 +1,4 @@
 defmodule Bunck do
-  defmodule Response, do: defstruct [:status, :headers, :body, :client]
-
-  defimpl Enumerable, for: Response do
-    defmodule ResponseList, do: defstruct [:list, :next_path, :client]
-
-    def count(_), do: {:error, __MODULE__}
-    def member?(_,_), do: {:error, __MODULE__}
-
-    def reduce(_, {:halt, acc}, _fun), do: {:halted, acc}
-    def reduce(response = %Response{}, acc, fun) do
-      reduce(%ResponseList{list: response.body["Response"], next_path: response.body["Pagination"]["older_url"], client: response.client}, acc, fun)
-    end
-    def reduce(response_list, {:suspend, acc}, fun), do: {:suspended, acc, &reduce(response_list, &1, fun)}
-    def reduce(response_list = %{list: [], next_path: nil}, {:cont, acc}, fun), do: {:done, acc}
-    def reduce(response_list = %{list: []}, {:cont, acc}, fun) do
-      {:ok, new_response} = %Bunck.GetPath{path: response_list.next_path} |> Bunck.request(response_list.client)
-      reduce(new_response, {:cont, acc}, fun)
-    end
-    def reduce(response_list = %{list: [h|t]}, {:cont, acc}, fun) do
-      reduce(%{response_list | list: t}, fun.(h, acc), fun)
-    end
-  end
-
   @moduledoc """
   Bunck is a client for the Bunq API.
 
@@ -52,7 +29,37 @@ defmodule Bunck do
     %Bunck.User.List{} |> Bunck.request(client) # get all users
     %Bunck.User.Get{user_id: 4} |> Bunck.request(client) # get user with id 4
   ```
+
+    You can also iterate over a response using `Enum`:
+  ```elixir
+    with {:ok, res} <- %Bunck.Payments.List{...} |> Bunck.request(client),
+    do: Enum.map(fun(payment) -> ... end)
+    ```
+  ```
   """
+
+  defmodule Response, do: defstruct [:status, :headers, :body, :client]
+
+  defimpl Enumerable, for: Response do
+    defmodule ResponseList, do: defstruct [:list, :next_path, :client]
+
+    def count(_), do: {:error, __MODULE__}
+    def member?(_,_), do: {:error, __MODULE__}
+
+    def reduce(_, {:halt, acc}, _fun), do: {:halted, acc}
+    def reduce(response = %Response{}, acc, fun) do
+      reduce(%ResponseList{list: response.body["Response"], next_path: response.body["Pagination"]["older_url"], client: response.client}, acc, fun)
+    end
+    def reduce(response_list, {:suspend, acc}, fun), do: {:suspended, acc, &reduce(response_list, &1, fun)}
+    def reduce(response_list = %{list: [], next_path: nil}, {:cont, acc}, fun), do: {:done, acc}
+    def reduce(response_list = %{list: []}, {:cont, acc}, fun) do
+      {:ok, new_response} = %Bunck.GetPath{path: response_list.next_path} |> Bunck.request(response_list.client)
+      reduce(new_response, {:cont, acc}, fun)
+    end
+    def reduce(response_list = %{list: [h|t]}, {:cont, acc}, fun) do
+      reduce(%{response_list | list: t}, fun.(h, acc), fun)
+    end
+  end
 
   def request(payload, client) do
     Bunck.Request.request(payload, client)
