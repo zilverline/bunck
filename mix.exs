d65dc385f1f7c8750b50afb827c1d43cc86ef901
Derek Kraan <derek.kraan@gmail.com>
2017-07-07 10:43:16 +0200

    bump version to 0.1.3

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Bunck.Mixfile do
 
   def project do
     [app: :bunck,
-     version: "0.1.2",
+     version: "0.1.3",
      build_path: "_build",
      config_path: "config/config.exs",
      deps_path: "deps",
